fceac03ceb16a8a5da03fcbe72e5c44cfaae5ed5
James <james@james-lubuntu-vm.(none)>
2012-10-26 20:11:18 -0500

    Some CSS updates

--- index.php
+++ index.php
@@ -7,22 +7,26 @@
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			
 			<div class="post">
+			 <div class="title-bar">
                 <!-- Display the Title as a link to the Post's permalink. -->
                 <h3>
                     <a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>">
                         <?php the_title(); ?>
                     </a>
                 </h3>
-
+                
                 <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
                 <div class="by-line">
                     <?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?>
                 </div>
+            </div>
 
                 <div class="entry">
                     <?php the_content(); ?>
                 </div>
-                
+                <div class="comment">
+                    <?php comments_popup_link('No Comments »', '1 Comment »', '% Comments »'); ?>
+                </div>
             </div>
 			
             <?php endwhile; else: ?>
--- single.php
+++ single.php
@@ -7,34 +7,31 @@
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			
 			<div class="post">
+			 <div class="title-bar">
                 <!-- Display the Title as a link to the Post's permalink. -->
                 <h3>
                     <a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>">
                         <?php the_title(); ?>
                     </a>
                 </h3>
-		
+                
                 <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
                 <div class="by-line">
                     <?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?>
                 </div>
+            </div>
 
-                <div class="entry">
-                    <?php the_content(); ?>
-                </div>
-                
-				<p class="postmetadata">
-				<?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
-				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
-				</p>
+            <div class="entry">
+                <?php the_content(); ?>
+            </div>
+            
+            <div class="comments-template">
+				<?php comments_template(); ?>
+			</div>
 
 			</div>
-			
 
-			<div class="comments-template">
-				<h2>What do you think?</h2>
-					<?php comments_template(); ?>
-			</div>
+
 			
 		</div>
 
