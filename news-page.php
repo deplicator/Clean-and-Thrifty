208e5336bae67143181696a854395f37b0deb329
James Pryor <james@geekwagon.net>
2012-10-28 20:51:00 -0500

    Cleaned up code. Added post navigation and header link.

--- news-page.php
+++ news-page.php
@@ -6,42 +6,24 @@ Template Name: News Page
 get_header();
 ?>
 
-    <div class="main">
-        <?php wp_nav_menu(); ?>
-	
-        <div id="content-column">
-            <?php query_posts('category_name=News&posts_per_page=10'); ?>
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			
-			<div class="post">
-			 <div class="title-bar">
-                <!-- Display the Title as a link to the Post's permalink. -->
-                <h3>
-                    <a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>">
-                        <?php the_title(); ?>
-                    </a>
-                </h3>
-                
-                <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
-                <div class="by-line">
-                    <?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?>
-                </div>
-            </div>
+<div class="main">
+    <?php wp_nav_menu(); ?>
 
-                <div class="entry">
-                    <?php the_content(); ?>
-                </div>
-                <div class="comment">
-                    <?php comments_popup_link('No Comments »', '1 Comment »', '% Comments »'); ?>
-                </div>
-            </div>
-			
-            <?php endwhile; else: ?>
-                <p><?php _e('Sorry, no posts labeled as \'News\'.'); ?></p>
-            <?php endif; ?>
+    <div id="content-column">
+        <?php query_posts('category_name=News'); ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        
+        <?php include 'post.php' ?>
+        
+        <?php endwhile; ?>
+            <div class="navigation">
+        		<?php posts_nav_link(' | '); ?>
+        	</div>
+    	<?php else: ?>
+            <p><?php _e('Sorry, nothing labeled as \'News\'.'); ?></p>
+        <?php endif; ?>
 
-		</div>
-	
 	</div>
+</div>
 
 <?php get_footer(); ?>
